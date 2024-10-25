274d5f21d41da4f598a06a452c245bbdd55bb75d
Lilian Santos <100161978+LilianMS@users.noreply.github.com>
2024-10-25 10:19:52 -0300

    update engine.js

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -25,6 +25,24 @@ const emojis = [
     const card = document.createElement('div');
     card.className = 'item';
     card.innerHTML = spawnEmojis[i];
+    card.onclick = handleClick;
     document.querySelector('.game').appendChild(card);
   }
   
+  function handleClick() {
+    if(openCards.length < 2) {
+      this.classList.add('cardOpen');
+      openCards.push(this);
+    }
+    if(openCards.length === 2) {
+      if(openCards[0].innerHTML === openCards[1].innerHTML) {
+        openCards.forEach(card => card.classList.add('cardMatch'));
+        openCards = [];
+      } else {
+        setTimeout(() => {
+          openCards.forEach(card => card.classList.remove('cardOpen'));
+          openCards = [];
+        }, 500);
+      }
+    }
+  }
